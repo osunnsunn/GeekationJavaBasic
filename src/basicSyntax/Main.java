00fde59fd8413f0c88fc3fd0c9835e59d527dce3
Shun Fukuda <osunnrnjoy@gmail.com>
2025-09-25 01:03:54 +0900

    DataType.javaの実装

---
+++ src/basicSyntax/Main.java
@@ -0,0 +1,27 @@
+package basicSyntax;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("こんにちは");
+	    System.out.println("私の名前は");
+	    System.out.println("Geek君です。");
+	    System.out.println("こんにちは" + "私の名前は" + "Geek君です。");
+		
+		System.out.println(1);
+	    System.out.println(-1);
+	    System.out.println(5 + 1);
+	    System.out.println("5 + 1");
+	    System.out.println("5" + "1");
+		
+		System.out.println(0.25);
+	    System.out.println(-5.0);
+	    System.out.println(5.0 - 0.25);
+	    
+	    System.out.println(true);
+	    System.out.println(false);
+
+	}
+
+}
