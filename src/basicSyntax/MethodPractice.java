7e268acb077d29f53876cdfbee75b1f0a386fa1b
Shun Fukuda <osunnrnjoy@gmail.com>
2025-10-01 13:11:18 +0900

    MethodPractice.javaの実装

---
+++ src/basicSyntax/MethodPractice.java
@@ -0,0 +1,30 @@
+package basicSyntax;
+
+public class MethodPractice {
+
+	public static void main(String[] args) {
+		int a = 6;
+		int b = 2;
+
+		System.out.println("計算結果は" + addition(a, b) + "です。");
+		System.out.println("計算結果は" + subtraction(a, b) + "です。");
+		System.out.println("計算結果は" + multiplication(a, b) + "です。");
+		System.out.println("計算結果は" + division(a, b) + "です。");
+	}
+
+	public static int addition(int a, int b) {
+		return a + b;
+	}
+
+	public static int subtraction(int a, int b) {
+		return a - b;
+	}
+
+	public static int multiplication(int a, int b) {
+		return a * b;
+	}
+
+	public static int division(int a, int b) {
+		return a / b;
+	}
+}
